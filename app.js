c010aa2bf7656bdcb5beec1b1f7c2d9a4d685232
igohs88 <igohs88@gmail.com>
2024-02-05 01:27:48 +0900

    add page

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
+
 
 var app = express();
 
@@ -18,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/hello',helloRouter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
